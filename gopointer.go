d6920ea9b17beeb845cc1ae44a697a9a00eabc1e
Bret Edwards <jbe@amazon.com>
2019-03-05 09:14:52 -0800

    function accepts indeterminate size slice. slice in main adjustable.

--- gopointer.go
+++ gopointer.go
@@ -6,16 +6,20 @@ import (
 	"time"
 )
 
-func printval(slicepntr *[10]int) (err error) {
-	for i := range slicepntr {
-		fmt.Printf("Value in slicepntr[%d] is %v\n", i, slicepntr[i])
+func printval(slicepntr *[]int) (err error) {
+	var slicearray = *slicepntr
+	for i := range slicearray {
+		fmt.Printf("Value in slicepntr[%d] is %v\n", i, slicearray[i])
 	}
-	fmt.Printf("The address of pointer slicepointer is %v", &slicepntr)
+	fmt.Printf("The address of pointer slicepointer is %p", slicepntr)
 	return nil
 }
 
 func main() {
-	var intarray [10]int
+	var (
+		intarray    = []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+		intarrayptr = &intarray
+	)
 
 	for i, val := range intarray {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -23,5 +27,6 @@ func main() {
 		intarray[i] = val
 		fmt.Println("intarray", i, "is", intarray[i])
 	}
+	fmt.Printf("Address for intarray is %p\n", intarrayptr)
 	printval(&intarray)
 }
